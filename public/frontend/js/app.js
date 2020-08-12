fcc2e2ca66365b04b632487808efc43b089e02bd
zainzulfiqar <50246160+zainzulifqargit@users.noreply.github.com>
2020-08-12 22:18:41 +0500

    home page new

--- public/frontend/js/app.js
+++ public/frontend/js/app.js
@@ -1,52 +1,80 @@
-setTimeout(()=>{
-    
-    
-    if (x.matches) { // If media query matches
-        document.getElementById('image1').style.left = "25%";
-        document.getElementById('image1').style.opacity = "1";
-        document.getElementById('image2').style.width = "194px"
-        document.getElementById('image2').style.opacity = "1";
-        document.getElementById('image2').style.left = "42%";
-        document.getElementById('image2').style.top= "109px";
 
-        document.getElementById('image3').style.left =  "68%";
-        document.getElementById('image3').style.top= "109px";
+setTimeout(() => {
 
-        document.getElementById('image3').style.opacity = "1";
-        document.getElementById('image4').style.left= "1.4%";
-        document.getElementById('image4').style.top= "356px";
-        document.getElementById('image4').style.opacity = "1";
-        document.getElementById('image5').style.top= "412px";
-        document.getElementById('image5').style.left= "26.3%";
-        document.getElementById('image5').style.opacity = "1";
-        document.getElementById('image6').style.width = "274px"
-        document.getElementById('image6').style.top= "232px";
-        document.getElementById('image6').style.left= "44.4%";
+    document.getElementById('image1').style.transform = "scale(1)"
+    document.getElementById('image1').style.opacity = "1"
 
-        document.getElementById('image6').style.opacity = "1";
-        document.getElementById('image7').style.left =  "59%";
-        document.getElementById('image7').style.opacity = "1";
-    } else {
-        
-        document.getElementById('image1').style.left = "31%";
-        document.getElementById('image1').style.opacity = "1";
-        document.getElementById('image2').style.width = "194px"
-        document.getElementById('image2').style.opacity = "1";
-        document.getElementById('image3').style.left =  "62%";
-        document.getElementById('image3').style.opacity = "1";
-        document.getElementById('image4').style.left= "14.4%";
-        document.getElementById('image4').style.opacity = "1";
-        document.getElementById('image5').style.top= "310px";
-        document.getElementById('image5').style.opacity = "1";
-        document.getElementById('image6').style.width = "264px"
-        document.getElementById('image6').style.opacity = "1";
-        document.getElementById('image7').style.left =  "58%";
-        document.getElementById('image7').style.opacity = "1";
-    }
-    
-    },500)
-    
-    var x = window.matchMedia("(max-width: 1024px)")
-    myFunction(x) // Call listener function at run time
-    x.addListener(myFunction) // Attach listener function on state changes
+    document.getElementById('image2').style.transform = "scale(1)"
+    document.getElementById('image2').style.opacity = "1"
+
+    document.getElementById('image3').style.transform = "scale(1)"
+    document.getElementById('image3').style.opacity = "1"
+
+    document.getElementById('image4').style.left = "11%"
+    document.getElementById('image4').style.opacity = "1"
+
+    document.getElementById('image5').style.left = "70.3%"
+    document.getElementById('image5').style.opacity = "1"
+
+    document.getElementById('image6').style.opacity = "1"
+    document.getElementById('image6').style.top = "-211px"
+
+    document.getElementById('image7').style.opacity = "1"
+    document.getElementById('image7').style.top = "-252px"
+
+    document.getElementById('image8').style.top = "109px"
+    document.getElementById('image8').style.opacity = "1"
+
+    document.getElementById('image9').style.left = "68%"
+    document.getElementById('image9').style.opacity = "1"
+
+    document.getElementById('image10').style.left = "8%"
+    document.getElementById('image10').style.opacity = "1"
+
+
+
+}, 500)
+
+
+function dropDownIn() {
+    document.getElementById("drop").style.opacity = "1"
+
+}
+function dropDownOut() {
+    document.getElementById("drop").style.opacity = "0"
+
+
+}
+
+
+setTimeout(() => {
+    document.getElementById('mobImage1').style.left = "0%"
+}, 500);
+setTimeout(() => {
+    document.getElementById('mobImage2').style.right = "0%"
+}, 1000);
+setTimeout(() => {
+    document.getElementById('mobImage3').style.left = "0%"
+}, 1500);
+setTimeout(() => {
+    document.getElementById('mobImage4').style.right = "0%"
+}, 2000);
+setTimeout(() => {
+    document.getElementById('mobImage5').style.left = "0%"
+}, 2500);
+setTimeout(() => {
+    document.getElementById('mobImage6').style.right = "0%"
+}, 3000);
+setTimeout(() => {
+    document.getElementById('mobImage7').style.left = "0%"
+}, 3500);
+setTimeout(() => {
+    document.getElementById('mobImage8').style.right = "0%"
+}, 4000);
+setTimeout(() => {
+    document.getElementById('mobImage9').style.left = "0%"
+}, 4500);
+setTimeout(() => {
+    document.getElementById('mobImage10').style.right = "0%"
+}, 5000);
 
--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -5,34 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Virtual Digital Experience</title>
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
+    
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/noframework.waypoints.min.js"
+        integrity="sha512-fHXRw0CXruAoINU11+hgqYvY/PcsOWzmj0QmcSOtjlJcqITbPyypc8cYpidjPurWpCnlB8VKfRwx6PIpASCUkQ=="
+        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     
 </head>
 <body>
     <nav>
         <div class="navbar">
             <div class="logo">
-            <div class="logoImg">
-                <img src="{{ asset('frontend/images/Asset 14@2x.png') }}" alt="">
+                <div class="logoImg">
+                    <img src="{{ asset('frontend/images/Asset 14@2x.png') }}" alt="">
+                </div>
+                <div class="logoTxt">
+                    <img src="{{ asset('frontend/images/Asset 12@2x.png') }}" alt="">
+                </div>
             </div>
-            <div class="logoTxt">
-             <img src="{{ asset('frontend/images/Asset 12@2x.png') }}" alt="">
-        </div>
-        </div>
-        <div class="icons">
-            <div>
-                <img src="{{ asset('frontend/images/Asset 8@2x.png') }}" alt="">
-            </div>
-            <div>
-                <img src="{{ asset('frontend/images/Asset 10@2x.png') }}" alt="">
-               
-            </div>
-            <div>
-                <img src="{{ asset('frontend/images/Asset 9@2x.png') }}" alt="">
-            </div>
+            <div class="icons">
 
+                <div class="hide">
+                    <img src="{{ asset('frontend/images/Asset 10@2x.png') }}" alt="">
+
+                </div>
+                <div onmouseover="dropDownIn()" onmouseout="dropDownOut()">
+                    <img src="{{ asset('frontend/images/Asset 9@2x.png') }}" alt="">
+                    <div id="drop" class="dropDown">
+                        <div>
+                            <p>ACTION</p>
+                        </div>
+                        <div>
+                            <p>VISIT</p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
         </div>
-    </div>
     </nav>
 
    @yield('content')
